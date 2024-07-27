176c98766f90b7e504bca872ec1e897fb2e0a11a
rosauraGuevara <rouscenturin@gmail.com>
2024-07-27 13:22:13 -0400

    corrección

--- python_heroku_task/urls.py
+++ python_heroku_task/urls.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create', views.create, name='create'),
-    path('detail/<int:article_id>', views.detail, name='detail'),
-    path('edit/<int:article_id>', views.edit, name='edit'),
-    path('delete/<int:article_id>', views.delete, name='delete'),
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
 ]
